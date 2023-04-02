9742de39feff87f166dc281f283a05985509af7b
unknown <anton_kho@msn.com>
2023-04-02 22:32:46 +0800

    added header component

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Provider as PaperProvider, Text } from "react-native-paper";
+import Header from "./app/components/Header";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Header />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/components/Header.js
@@ -0,0 +1,42 @@
+import { ScrollView, Text, View } from "react-native";
+import { Button, Appbar } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "@env";
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/categories`);
+      setCategories(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadCategories();
+    console.log(categories);
+  }, []);
+
+  return (
+    <Appbar.Header>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {categories.map((item) => (
+          <Button
+            key={item._id}
+            mode="outlined"
+            compact="true"
+            style={{ marginHorizontal: 5 }}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </ScrollView>
+    </Appbar.Header>
+  );
+};
+
+export default Header;
