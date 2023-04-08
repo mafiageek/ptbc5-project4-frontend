532641868f6b18ade98446d608719634f191d484
unknown <anton_kho@msn.com>
2023-04-08 22:39:37 +0800

    add stack navigator

--- App.js
+++ App.js
@@ -2,21 +2,26 @@ import * as React from "react";
 import { Provider as PaperProvider, Text } from "react-native-paper";
 import MainContainer from "./app/MainContainer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import FeedScreen from "./app/screens/FeedScreen";
+import AddScreen from "./app/screens/AddScreen";
+
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   <Stack.Navigator>
     <Stack.Screen name="Feed" component={FeedScreen} />
+    <Stack.Screen name="Add" component={AddScreen} />
   </Stack.Navigator>;
 };
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <NavigationContainer>
+      <PaperProvider>
+        <StackNavigator />
         <MainContainer />
-      </NavigationContainer>
-    </PaperProvider>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
