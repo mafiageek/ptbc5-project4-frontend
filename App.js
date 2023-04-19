0de2828801f6893a1cde2dc467555613cce1c599
unknown <anton_kho@msn.com>
2023-04-19 23:16:23 +0800

    working with notification

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Provider as PaperProvider, Text } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./app/context/auth";
 import MainNavigator from "./app/navigation/MainNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <NavigationContainer>
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,14 +1,16 @@
 import { FlatList, RefreshControl, View, SafeAreaView } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-
+import * as Notifications from "expo-notifications";
 import Header from "../components/Header";
 import Listings from "../components/Listings";
 import { BASE_URL } from "@env";
+import { useAuth } from "../context/auth";
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [auth, setAuth] = useAuth();
 
   const loadListings = async (catId) => {
     try {
@@ -29,6 +31,35 @@ const ListingsScreen = ({ navigation }) => {
       console.log(err);
     }
   };
+
+  async function configurePushNotifications() {
+    const { status } = await Notifications.getPermissionsAsync();
+    let finalStatus = status;
+
+    if (finalStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+
+    Notifications.getExpoPushTokenAsync({
+      projectId: "510f515f-d157-4c62-95d5-015caaaa1993",
+    })
+      .then(({ data }) => {
+        axios.patch(`${BASE_URL}/user/${auth.uid}`, {
+          pushtoken: data,
+        });
+      })
+      .catch((err) => console.log(error));
+  }
+
+  useEffect(() => {
+    configurePushNotifications();
+  }, []);
 
   useEffect(() => {
     loadListings();
